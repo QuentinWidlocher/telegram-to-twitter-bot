d269b7a409101ddd864c03e0aeec2dbb2039d175
QuentinWidlocher <qwidlocher@gmail.com>
2022-07-14 20:06:21 +0200

    🥅  Catch errors

--- src/routes/authorize.ts
+++ src/routes/authorize.ts
@@ -7,53 +7,58 @@ import { createHandled } from "../utils/error-handling";
 
 const token = process.env.TELEGRAM_BOT_TOKEN!;
 
+const redirect = {
+  statusCode: 302,
+  headers: {
+    Location: 'tg://resolve?domain=TgToTwitterBot',
+  },
+}
+
 export const handler: Handler = createHandled(async (event) => {
   console.log("event", event);
 
   const bot = new TelegramBot(token);
 
-  const oauthToken = event.queryStringParameters?.oauth_token;
-  const oauthVerifier = event.queryStringParameters?.oauth_verifier;
-  const userId = event.queryStringParameters?.userId;
-  const chatId = event.queryStringParameters?.chatId;
-
-  invariant(oauthToken, "oauthToken is required");
-  invariant(oauthVerifier, "oauthVerifier is required");
-  invariant(userId, "userId is required");
-  invariant(chatId, "chatId is required");
-
-  const redirect = {
-    statusCode: 302,
-    headers: {
-      Location: 'tg://resolve?domain=TgToTwitterBot',
-    },
-  }
-
-  let { credentials } = await retreive(userId)
-
-  invariant(credentials?.authLink, "credentials.authLink is required");
-
   try {
-    let authentication = await getClient(oauthToken, credentials.authLink.oauth_token_secret, oauthVerifier);
-    await update(userId, {
-      credentials: {
-        ...credentials,
-        accessSecret: authentication.accessSecret,
-        accessToken: authentication.accessToken,
-        oauthVerifier: oauthVerifier,
-      }
-    })
 
-    await bot.sendMessage(chatId, `
+    const oauthToken = event.queryStringParameters?.oauth_token;
+    const oauthVerifier = event.queryStringParameters?.oauth_verifier;
+    const userId = event.queryStringParameters?.userId;
+    const chatId = event.queryStringParameters?.chatId;
+
+    invariant(oauthToken, "oauthToken is required");
+    invariant(oauthVerifier, "oauthVerifier is required");
+    invariant(userId, "userId is required");
+    invariant(chatId, "chatId is required");
+
+    let { credentials } = await retreive(userId)
+
+    try {
+      invariant(credentials?.authLink, "credentials.authLink is required");
+
+      let authentication = await getClient(oauthToken, credentials.authLink.oauth_token_secret, oauthVerifier);
+      await update(userId, {
+        credentials: {
+          ...credentials,
+          accessSecret: authentication.accessSecret,
+          accessToken: authentication.accessToken,
+          oauthVerifier: oauthVerifier,
+        }
+      })
+
+      await bot.sendMessage(chatId, `
 You've just connected your Twitter account to this bot.
 Now, call the command \`/link @<channel-name>\` where @channel-name is the name of the Telegram channel you want to link.
       `);
-  } catch (error) {
-    console.log("error", error);
-    await bot.sendMessage(chatId, `
+    } catch (error) {
+      console.error("error", error);
+      await bot.sendMessage(chatId, `
 An error occured while connecting your Twitter account to this bot.
 Please try again.
       `);
+    }
+  } catch (error) {
+    console.log("error", error);
   }
 
   return redirect;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -17,57 +17,63 @@ export const handler: Handler = createHandled(async (event) => {
   try {
     const body = JSON.parse(event.body);
 
-    await new Promise<void>(resolve => {
+    await new Promise<void>((resolve, reject) => {
       bot.onText(/^(?!\/)(.*)/m, async (msg) => {
         console.log("msg", msg);
-        invariant(msg.text, "msg.text is required");
-        invariant(msg.from?.id, "msg.from.id is required");
+        try {
+          invariant(msg.text, "msg.text is required");
+          invariant(msg.from?.id, "msg.from.id is required");
 
-        let userData = await retreive(msg.from.id)
+          let userData = await retreive(msg.from.id)
 
-        invariant(userData?.credentials?.accessToken, "userData.credentials.accessToken is required");
-        invariant(userData?.credentials?.accessSecret, "userData.credentials.accessSecret is required");
-        invariant(userData?.credentials?.oauthVerifier, "userData.credentials.oauthVerifier is required");
-        invariant(userData.channelId, "userData.channelId is required");
+          invariant(userData?.credentials?.accessToken, "userData.credentials.accessToken is required");
+          invariant(userData?.credentials?.accessSecret, "userData.credentials.accessSecret is required");
+          invariant(userData?.credentials?.oauthVerifier, "userData.credentials.oauthVerifier is required");
+          invariant(userData.channelId, "userData.channelId is required");
 
-        let { client: twitterClient } = await getClient(userData.credentials.accessToken, userData.credentials.accessSecret, userData.credentials.oauthVerifier);
+          let { client: twitterClient } = await getClient(userData.credentials.accessToken, userData.credentials.accessSecret, userData.credentials.oauthVerifier);
 
-        let [tgRes, twRes] = await Promise.all([
-          bot.sendMessage(userData.channelId, msg.text),
-          twitterClient.v2.tweet(msg.text),
-        ])
+          let [tgRes, twRes] = await Promise.all([
+            bot.sendMessage(userData.channelId, msg.text),
+            twitterClient.v2.tweet(msg.text),
+          ])
 
-        if (twRes.errors) {
-          console.error("twRes.errors", twRes.errors);
-          await bot.sendMessage(userData.channelId, "Error: " + twRes.errors.join('\n'));
-          resolve();
-          return;
+          if (twRes.errors) {
+            console.error("twRes.errors", twRes.errors);
+            await bot.sendMessage(userData.channelId, "Error: " + twRes.errors.join('\n'));
+            resolve();
+            return;
+          }
+
+          console.log("twRes.data", twRes.data);
+
+          await bot.sendMessage(msg.from.id, "Message sent"),
+
+            resolve();
+        } catch (error) {
+          console.error("error", error);
+          reject(error);
         }
-
-        console.log("twRes.data", twRes.data);
-
-        await bot.sendMessage(msg.from.id, "Message sent"),
-
-          resolve();
       })
 
       bot.onText(/\/start$/m, async (msg) => {
-        console.log("/start", msg);
-        invariant(msg.from?.id, "msg.from.id is required");
+        try {
+          console.log("/start", msg);
+          invariant(msg.from?.id, "msg.from.id is required");
 
-        let authLink = await startLogin({ userId: msg.from.id, chatId: msg.chat.id });
+          let authLink = await startLogin({ userId: msg.from.id, chatId: msg.chat.id });
 
-        console.debug('authLink', authLink)
+          console.debug('authLink', authLink)
 
-        await update(msg.from.id, {
-          credentials: {
-            authLink
-          }
-        })
+          await update(msg.from.id, {
+            credentials: {
+              authLink
+            }
+          })
 
-        console.log("updated db");
+          console.log("updated db");
 
-        await await bot.sendMessage(msg.chat.id, `
+          await await bot.sendMessage(msg.chat.id, `
 Hey ! Welcome to this bot 👋
 
 You can use it to bind a Twitter account to a Telegram channel.
@@ -76,55 +82,64 @@ Each time you send a post in the channel, it will be sent to your Twitter accoun
 To start, you'll need to connect your Twitter account.
 Click the link below to get started. You'll be redirected to this bot and asked to press start again.
     `, {
-          reply_markup: {
-            inline_keyboard: [
-              [{ text: "Connect this bot to Twitter", url: authLink.url }],
-            ],
-          },
-        });
+            reply_markup: {
+              inline_keyboard: [
+                [{ text: "Connect this bot to Twitter", url: authLink.url }],
+              ],
+            },
+          });
 
-        resolve();
+          resolve();
+        } catch (error) {
+          console.error("error", error);
+          reject(error);
+        }
       });
 
       bot.onText(/^\/link\s(.*)/m, async (msg, match) => {
-        console.log("/link <channel>", msg, match);
-        if (!msg.from?.id) {
-          resolve();
-          return
-        }
+        try {
+          console.log("/link <channel>", msg, match);
+          if (!msg.from?.id) {
+            resolve();
+            return
+          }
 
-        const channelName = (match ?? [])[1];
-        console.log("channelName", channelName);
+          const channelName = (match ?? [])[1];
+          console.log("channelName", channelName);
 
-        if (!channelName) {
-          await bot.sendMessage(msg.chat.id, `
+          if (!channelName) {
+            await bot.sendMessage(msg.chat.id, `
 You need to provide a channel name.
 Call the command \`/link @<channel-name>\` where @channel-name is the name of the Telegram channel you want to link.
       `);
-          resolve();
-          return
-        }
+            resolve();
+            return
+          }
 
-        let { credentials } = await retreive(msg.from.id)
+          let { credentials } = await retreive(msg.from.id)
 
-        if (!credentials?.authLink?.oauth_token) {
-          await bot.sendMessage(msg.chat.id, `
+          if (!credentials?.authLink?.oauth_token) {
+            await bot.sendMessage(msg.chat.id, `
 You need to connect your Twitter account first.
 Call the command \`/start\` to start the process.
       `);
-          resolve();
-          return
-        }
+            resolve();
+            return
+          }
 
-        await update(msg.from.id, {
-          channelId: channelName,
-        })
+          await update(msg.from.id, {
+            channelId: channelName,
+          })
 
-        await bot.sendMessage(msg.chat.id, `
+          await bot.sendMessage(msg.chat.id, `
 You've just linked your Twitter account to this bot.
 Now, you can add this bot to you channel, and when you send posts, they will be synced with your Twitter account.
     `);
-        resolve();
+          resolve();
+        } catch (error) {
+          console.error("error", error);
+          reject(error);
+        }
       })
 
       bot.processUpdate(body);
